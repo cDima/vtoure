a9b59389e874bc75332dc8da8ba8b51d2ad4b6cc
Dmitry Sadakov <sadakov@gmail.com>
2014-05-03 11:47:31 -0400

    Adding argument lookup global func
    because ng-route might be overkill

--- javascript/global.js
+++ javascript/global.js
@@ -78,3 +78,14 @@ function lookupContains(array, prop, pattern) {
         if (array[i][prop].match(reg) != null) return array[i];
     return null;
 }
+
+//$routeParams a bit too expensive
+function getQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split("=");
+        if (pair[0] == variable) { return pair[1]; }
+    }
+    return (false);
+}
